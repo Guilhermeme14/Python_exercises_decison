f619ee8954e3696d0f5f8b03a36414b600d9060f
Guilherme Fernandes <guifernandes111@gmail.com>
2024-04-01 16:50:00 -0300

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+n1 = input("Digite o primeiro numero: ")
+n2 = input("Digite o segundo numero: ")
+
+if (n1 > n2):
+    print(n1)
+elif (n2 > n1):
+    print(n2)
+else:
+    print("Os números são iguais")
