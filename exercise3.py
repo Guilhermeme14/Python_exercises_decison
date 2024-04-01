fab55f7de4fa75f00019aff8bbdb6b9b62140e86
Guilherme Fernandes <guifernandes111@gmail.com>
2024-04-01 16:50:19 -0300

    Add files via upload

---
+++ exercise3.py
@@ -0,0 +1,8 @@
+l = input("Qual o seu sexo F ou M? ")
+
+if l == "F":
+    print("Seu sexo é feminino!")
+elif l == "M":
+    print("Seu sexo é masculino!")
+else:
+    print("Digitou incorretamente")
