0f858c4647e9f19d01e6986ae42311d1c92158f2
Guilherme Fernandes <guifernandes111@gmail.com>
2024-04-04 16:55:49 -0300

    Add files via upload

---
+++ exercise10.py
@@ -0,0 +1,14 @@
+turno = input("Qual o seu turno?")
+
+if turno == "m":
+    print("Bom dia!")
+
+elif turno == "v":
+    print("Boa tarde!")
+
+elif turno == "n":
+    print("boa noite!")
+
+else: 
+    print("Valor inválido")
+    
